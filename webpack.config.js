be44acee5c70d74218223b63328cfe0d08c27e66
ThaddG <thaddious.gorges@gmail.com>
2021-06-21 00:01:36 -0400

    jsx files no longer cause error

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,50 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/test.js',
-  module: {},
+  entry: {
+    popup: path.resolve('src/popup/popup.jsx'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'source-map-loader',
+          },
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve('src/manifest.json'),
+          to: path.resolve('dist'),
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      title: 'React Chrome Extension',
+      filename: 'popup.html',
+      chunks: ['popup'],
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
   // where to place files once it's done being built
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    path: path.resolve('dist'),
+    clean: true,
   },
 };
