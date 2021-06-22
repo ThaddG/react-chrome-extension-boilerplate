ee53efb66077d7fdc63f3d571f84b47ab6d2c412
ThaddG <thaddious.gorges@gmail.com>
2021-06-21 21:52:39 -0400

    options page shows text

--- src/options/options.jsx
+++ src/options/options.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default function Options() {
   return (
     <div>
-      
+      <h1>Options Page</h1>
     </div>
   )
 }
+
+const root = document.createElement('div');
+document.body.appendChild(root);
+ReactDOM.render(<Options />, root);
